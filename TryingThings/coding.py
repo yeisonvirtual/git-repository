4c1258c1c6644d5767a49d7174ad17a9251aff21
Luis Marquez <englamc@gmail.com>
2023-05-30 15:13:18 -0400

    operations

--- TryingThings/coding.py
+++ TryingThings/coding.py
@@ -1,3 +1,30 @@
+def add_2_numbers(a:int, b:int):
+    result:int = a + b
+    print(str(a) + " + " + str(b))
+    print("Result: " + str(result))
+
+def substract_2_numbers(a:int, b:int):
+    result:int = a - b
+    print(str(a) + " - " + str(b))
+    print("Result: " + str(result))
+
+def multiply_2_numbers(a:int, b:int):
+    result:int = a * b
+    print(str(a) + " * " + str(b))
+    print("Result: " + str(result))
+
+def divide_2_numbers(a:int, b:int):
+    if b == 0:
+        print("Division by zero doesn't exist")
+    else:
+        result:int = a / b
+        print(str(a) + " / " + str(b))
+        print("Result: " + str(result))
+
+def ask_for_an_int():
+    number:int = int(input("Type an interger number: "))
+    return number
+
 def waving_to(name:str):
     print("Hello " + name + "!")
 
