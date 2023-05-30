b7003057c71ea9aaeb6daa65b588cb2f84b9b0f5
eulises <eulisesbrazon@gmail.com>
2023-05-30 14:56:57 -0400

    testing conflicts

--- TryingThings/coding.py
+++ TryingThings/coding.py
@@ -2,7 +2,7 @@ def waving_to(name:str):
     print("Hello " + name + "!")
 
 def run():
-    name:str = str(input("Type your name: "))
+    name = input("Type your name: ")
     waving_to(name)
 
 if __name__ == "__main__":
